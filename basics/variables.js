aef04c394d216694fb294487a10bba7832e1b7f5
ritika2026 <149147064+ritika2026@users.noreply.github.com>
2024-09-05 17:47:39 +0000

    container config done

---
+++ basics/variables.js
@@ -0,0 +1,9 @@
+console.log("hello world! its my first day of java script learning");
+const bday=2;
+let age=21;
+var year;
+age=22;
+console.log("after a year age will be",age);
+console.log(bday);
+console.table([bday,age,year]);
+//avoid use of var keyword coz of functional scope issue
