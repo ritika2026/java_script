0a9661fea2dc28e5e8d22bf770337216cdede580
ritika2026 <149147064+ritika2026@users.noreply.github.com>
2024-09-06 18:08:59 +0000

    container config done

---
+++ basics/datatype_convert.js
@@ -0,0 +1,14 @@
+// Number,string,undefined,null,boolean,bigint are primitive datatypes in js
+let a=6
+console.log(typeof(a));
+let a_in_string=String(a)
+console.log(a_in_string);
+console.log(typeof(a_in_string));
+let b="ritu0902"
+//console.log(typeof(b))
+let b_in_number=String(b)
+console.log(typeof(b_in_number));
+console.log(b_in_number);
+let score="ritika"
+let scorein_num=Number(score)
+console.log(scorein_num);
---
+++ basics/post_pre.js
@@ -0,0 +1,19 @@
+// let x=4;
+// x++;
+// console.log(x);
+// let y=8;
+// ++y;
+// console.log(y);
+
+let x = 3;
+const y = x++;
+console.log(y);
+//returns value before incrementing
+console.log("2"==2);
+console.log("3"===3);
+//datatypes are of two types on the basis of how they are assigned memory 
+//i.e. primitive (call by value based)and non primitive(reference based)
+//primitive-->String,Number,Boolean,undefined,null,Symbol,BigInt
+//non-primitive-->arrays,functions,objects
+//object is in form of key value pair
+//null datatype will be object on using typeof
